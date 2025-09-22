bb0576d5083a333526780c384d90c2c92fa89d52
Project516 <138796702+Project516@users.noreply.github.com>
2025-09-22 12:51:58 +0000

    still fails

--- joinleave/src/test/java/io/github/project516/JoinLeaveTest.java
+++ joinleave/src/test/java/io/github/project516/JoinLeaveTest.java
@@ -2,19 +2,21 @@ package io.github.project516;
 
 import org.mockbukkit.mockbukkit.MockBukkit;
 import org.mockbukkit.mockbukkit.ServerMock;
+import org.mockbukkit.mockbukkit.PlayerMock;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class joinleavetest {
+class JoinLeaveTest {
 
     World world;
+    ServerMock server;
 
     @BeforeEach
     void setUp() {
-        ServerMock mock = MockBukkit.mock();
+        server = MockBukkit.mock();
         joinleave plugin = MockBukkit.load(joinleave.class);
         this.world = mock.addSimpleWorld("test");
     }
@@ -27,5 +29,7 @@ class joinleavetest {
     @Test
     void test() {
         world.createExplosion(new Location(world, 0, 0, 0), 0.2f);
+        PlayerMock player = server.addPlayer();
+        player.disconnect();
     }
 }
