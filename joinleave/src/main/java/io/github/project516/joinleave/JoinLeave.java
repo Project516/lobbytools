0944fbcc519199d5f5da776cdd1ae402f2eea56a
Project516 <138796702+Project516@users.noreply.github.com>
2025-09-26 13:17:02 +0000

    reinit

--- joinleave/src/main/java/io/github/project516/joinleave/JoinLeave.java
+++ joinleave/src/main/java/io/github/project516/joinleave/JoinLeave.java
@@ -1,53 +1,21 @@
 package io.github.project516.joinleave;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class JoinLeave extends JavaPlugin implements Listener {
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        // Broadcast a join message to all players
-        Component joinMessage =
-                Component.text()
-                        .append(Component.text("[+]").color(NamedTextColor.GREEN))
-                        .append(Component.text(" "))
-                        .append(Component.text(event.getPlayer().getName()))
-                        .append(Component.text(" joined the game!"))
-                        .color(NamedTextColor.YELLOW)
-                        .build();
-
-        Bukkit.broadcast(joinMessage);
-
-        // Send a private welcome message to the joining player
-        event.getPlayer()
-                .sendMessage(
-                        Component.text()
-                                .append(Component.text("Welcome to the server, "))
-                                .append(Component.text(event.getPlayer().getName()))
-                                .append(Component.text("!"))
-                                .color(NamedTextColor.AQUA)
-                                .build());
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        // Broadcast a leave message to all players
-        Component leaveMessage =
-                Component.text()
-                        .append(Component.text("[-]").color(NamedTextColor.RED))
-                        .append(Component.text(" "))
-                        .append(Component.text(event.getPlayer().getName()))
-                        .append(Component.text(" left the game!"))
-                        .color(NamedTextColor.YELLOW)
-                        .build();
-
-        Bukkit.broadcast(leaveMessage);
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer()
+                .sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
     }
 }
