e650fd21567b9632a77319c6348e99cc73e0ef85
Project516 <138796702+Project516@users.noreply.github.com>
2025-09-29 18:42:49 +0000

    chatcalc init

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 cd joinleave
-./gradlew :spotlessApply
 ./gradlew build
 cd ..
 cp joinleave/build/libs/joinleave.jar builds/joinleave.jar
+
+cd chatcalc
+./gradlew build
+cd ..
+cp chatcalc/build/libs/chatcalc.jar builds/chatcalc.jar
